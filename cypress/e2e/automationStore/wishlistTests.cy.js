e1af1abb95dbdcc7596261fe6c52c6aa56a1907d
mirela tifui <mirela.tifui@endava.com>
2024-11-21 17:16:29 +0200

    updated the wishlist tests and the yml file to run all tests

--- cypress/e2e/automationStore/wishlistTests.cy.js
+++ cypress/e2e/automationStore/wishlistTests.cy.js
@@ -1,11 +1,12 @@
+import { beforeEach } from "mocha";
 import { validUser } from "../../constants/credentials";
-import { expectedText } from "../../constants/notificationAndMessages";
 
-
-describe('Cheks wishlist add / remove and update functionality', () => {
-    it('Checks add to wishlits', () => {
-        const productId = 68;
+describe(['smoke'], 'Checks wishlist add / remove and update functionality', () => {
+    const productId = 68;
+    beforeEach('Login', () => {
         cy.login(validUser.userName, validUser.password)
+    })
+    it('Checks add to wishlist', () => {
         cy.addProductToWishlist(productId).then((response) => {
             expect(response.status).to.eq(200);
             expect(response.body).to.contain('success":"Added to wish list')
@@ -15,9 +16,12 @@ describe('Cheks wishlist add / remove and update functionality', () => {
             expect(response.body).to.contain(`product_id=${productId}`);
             expect(response.body).to.contain('Absolute Anti-Age Spot Replenishing Unifying TreatmentSPF 15');
         });
+    });
+
+    it('Checks the remove from wishlits', () => {
         cy.removeItemFromWishlist(productId).then((response) => {
             expect(response.status).to.eq(200);
             expect(response.body).to.eq(`{"success":"Removed from wish list"}`)
         })
-    });
+    })
 });
