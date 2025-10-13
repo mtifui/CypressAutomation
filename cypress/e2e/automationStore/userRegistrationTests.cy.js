dec9c465319ce639914930e07937ffb14764dacd
mirela tifui <tifui_mirela@hotmail.com>
2025-10-13 13:06:58 +0300

    Fix create account selectors (#5)
    * Fix create account selectors
    
    * Stabilize registration e2e flow
    
    ---------
    
    Co-authored-by: mirela tifui <mirela.tifui@endava.com>

--- cypress/e2e/automationStore/userRegistrationTests.cy.js
+++ cypress/e2e/automationStore/userRegistrationTests.cy.js
@@ -3,10 +3,11 @@ import { urls } from "../../constants/urls";
 
 import { createAccountPage } from "../../pages/automationStoreElements";
 import { loginPage } from "../../pages/automationStoreElements";
-const counter = Math.floor((Math.random() * 1000)) + 1
 
 describe(['smoke'], 'Cheks the registration option', () => {
     it('checks that a new user can register when using all valid data', () => {
+        const uniqueSuffix = `${Date.now()}${Math.floor(Math.random() * 1000)}`;
+
         cy.visit(urls.accountLoginPage)
             .get(loginPage.pageTitle)
             .should('have.text', " Account Login");
@@ -19,33 +20,22 @@ describe(['smoke'], 'Cheks the registration option', () => {
         cy.visit(urls.createAccountPage)
             .get(createAccountPage.firstnameInput).type("Automation")
             .get(createAccountPage.lastnameInput).type("Test")
-            .get(createAccountPage.emailInput).type(`test@test${counter}.com`)
+            .get(createAccountPage.emailInput).type(`test${uniqueSuffix}@test.com`)
             .get(createAccountPage.address1Input).type("na, na ,na")
             .get(createAccountPage.cityInput).type("Iasi")
             .get(createAccountPage.countrySelect).select("Romania")
             .get(createAccountPage.regionSelect).select("Iasi")
             .get(createAccountPage.zipCodeInput).type("123456")
-            .get(createAccountPage.loginNameInput).type(`autotest${counter}`)
+            .get(createAccountPage.loginNameInput).type(`autotest${uniqueSuffix}`)
             .get(createAccountPage.passwordInput).type("User1")
             .get(createAccountPage.passwordConfirmInput).type("User1")
             .get(createAccountPage.agreeToTermsCheckbox).check();
+        cy.intercept('POST', 'https://automationteststore.com/index.php?rt=account/create').as('createUserRequest');
         cy.get(createAccountPage.continueButton).click({ force: true, timeout: 0 });
-        cy.intercept('POST', 'https://automationteststore.com/index.php?rt=account/create', {
-            statusCode: 200,
-            body: { "success": true, "message": "User account successfully created" },
-        }).as('createUserResponse');
+        cy.wait('@createUserRequest');
 
-        (interception) => {
-            expect(interception.response.statusCode).to.equal(200);
-            expect(interception.response.body).to.have.property('success', true);
-            expect(interception.response.body).to.have.property('message', 'User account successfully created');
-        };
-
-        cy.get('span.maintext')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.eq('Your Account Has Been Created!')
-            });
+        cy.get(createAccountPage.pageTitle, { timeout: 10000 })
+            .should('contain.text', 'Your Account Has Been Created!');
     })
 })
 
